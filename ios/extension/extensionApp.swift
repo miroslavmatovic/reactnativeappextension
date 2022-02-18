1fff2e3c7e2ce30a09238301b91ca000fa7dbb8d
Miroslav <miroslavmatovic@github.com>
2022-02-18 14:57:43 +0300

    first commit

---
+++ ios/extension/extensionApp.swift
@@ -0,0 +1,17 @@
+//
+//  extensionApp.swift
+//  extension
+//
+//  Created by Admin on 18.02.2022.
+//
+
+import SwiftUI
+
+@main
+struct extensionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
