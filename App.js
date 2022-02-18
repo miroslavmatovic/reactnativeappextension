98b3a9995b3a56ff30079f3079afba58c55d1ac9
Miroslav <miroslavmatovic@github.com>
2022-02-18 15:17:32 +0300

    debug

--- App.js
+++ App.js
@@ -3,6 +3,9 @@ import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
+  if (__DEV__) {
+    console.log("I am in debug");
+  }
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
